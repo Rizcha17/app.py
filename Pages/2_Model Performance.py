b3ed851f4a5ede7cdb4690aee47188548655829a
Rizcha17 <rizchalevianaputri@students.unnes.ac.id>
2025-05-22 09:10:57 +0700

    Create 2_Model Performance.py

---
+++ Pages/2_Model Performance.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuaracy_score
+
+st.set_page_config(page_title"Model Performance")
+st.title("Model Performance")
+st.header("Model Performance")
