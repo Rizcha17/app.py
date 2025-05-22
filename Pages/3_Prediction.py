123ff6a33bbb2fbf28de026fc4115e010157eb16
Rizcha17 <rizchalevianaputri@students.unnes.ac.id>
2025-05-22 09:13:56 +0700

    Update 3_Prediction.py

--- Pages/3_Prediction.py
+++ Pages/3_Prediction.py
@@ -1,1 +1,9 @@
-Model Performance
+import streamlit as st
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuaracy_score
+
+st.set_page_config(page_title"predictions")
+st.title("predictions")
+st.header("predictions")
