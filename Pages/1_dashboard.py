0a4eb51c223c2d615c699028d3ea292f8be20d9e
Rizcha17 <rizchalevianaputri@students.unnes.ac.id>
2025-05-22 09:13:34 +0700

    Update 1_dashboard.py

--- Pages/1_dashboard.py
+++ Pages/1_dashboard.py
@@ -4,6 +4,6 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, accuaracy_score
 
-st.set_page_config(page_title"predictions")
-st.title("predictions")
-st.header("predictions")
+st.set_page_config(page_title"Dashboard")
+st.title("Dashboard")
+st.header("Dashboard")
